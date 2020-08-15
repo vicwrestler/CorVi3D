9961434002cd6b8fc37fb91fe9f6f92f68029366
Vicwrestler <w-restler@hotmail.com>
2020-08-15 01:26:50 -0500

    Version 1 de CorVi3d

---
+++ js/prueba.js
@@ -0,0 +1,10 @@
+// var x = document.getElementsByClassName("img2");
+const x = document.getElementById("img2");
+x.onclick = () => {
+  x.src = "../img/imagenes/images.jpeg";
+};
+const prueba = () => {
+  console.log("hola mundo");
+};
+
+prueba();
