da261ccd0bbd02fd857d7b6f0f74a31c50252a14
Vicwrestler <w-restler@hotmail.com>
2020-08-17 02:15:55 -0500

    prueba para gh-pages

--- js/prueba.js
+++ js/prueba.js
@@ -1,5 +1,5 @@
 const what = document.getElementById("whats");
-what.addEventListener('mouseover', ()=>{
+what.addEventListener('mouseover', () => {
   what.style.color = "green";
 });
 what.addEventListener('mouseleave', () => {
