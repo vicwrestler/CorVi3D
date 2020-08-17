c0b76d83450dc9a2208ae29d829abf61b828bb60
Vicwrestler <w-restler@hotmail.com>
2020-08-17 03:41:41 -0500

    actualizacion lunes 3:41 am

--- js/prueba-dist.js
+++ js/prueba-dist.js
@@ -1,7 +1,13 @@
 const what = document.getElementById("whats");
+const servicio1 = document.getElementById("ser-im1");
+const modal1 = document.getElementById("modal1");
 what.addEventListener('mouseover', () => {
   what.style.color = "green";
 });
 what.addEventListener('mouseleave', () => {
   what.style.color = "black";
 });
+
+servicio1.addEventListener("click", ()=>{
+  modal1.classList.add("show");
+})
